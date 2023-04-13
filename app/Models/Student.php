84b667fba0203e356c962dd292ab4f55b1fe9ccf
Fábio Augusto Rodrigues <69022068+FabioAugustoRodrigues@users.noreply.github.com>
2023-04-13 11:48:19 -0300

    feat: created the Student repository

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -2,13 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class Student extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
---
+++ app/Repositories/StudentRepository.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Student;
+
+class StudentRepository
+{
+    protected $model;
+
+    public function __construct(Student $model)
+    {
+        $this->model = $model;
+    }
+
+    public function getAll()
+    {
+        return $this->model->all();
+    }
+
+    public function getById($id)
+    {
+        return $this->model->find($id);
+    }
+
+    public function findBySn($sn)
+    {
+        return $this->model->where('sn', $sn)->first();
+    }
+
+    public function findByEmail($email)
+    {
+        return $this->model->where('email', $email)->first();
+    }
+
+    public function create(array $data)
+    {
+        return $this->model->create($data);
+    }
+
+    public function update($id, array $data)
+    {
+        $record = $this->getById($id);
+        $record->update($data);
+
+        return $record->refresh();
+    }
+
+    public function delete($id)
+    {
+        $record = $this->getById($id);
+
+        return $record->delete();
+    }
+}
