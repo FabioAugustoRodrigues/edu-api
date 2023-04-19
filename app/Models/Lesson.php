f2df6c7a6ac7b2a30a5754e1da0fc9384d5790ed
Fábio Augusto Rodrigues <69022068+FabioAugustoRodrigues@users.noreply.github.com>
2023-04-19 10:55:36 -0300

    feat: created the Lesson repository

--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Note extends Model
+class Lesson extends Model
 {
     use HasFactory;
 
---
+++ app/Repositories/LessonRepository.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Lesson;
+
+class LessonRepository
+{
+    protected $model;
+
+    public function __construct(Lesson $model)
+    {
+        $this->model = $model;
+    }
+
+    public function getAll()
+    {
+        return $this->model->all();
+    }
+
+    public function getById($id)
+    {
+        return $this->model->find($id);
+    }
+
+    public function create(array $data)
+    {
+        return $this->model->create($data);
+    }
+
+    public function update($id, array $data)
+    {
+        $record = $this->getById($id);
+        $record->update($data);
+
+        return $record->refresh();
+    }
+
+    public function delete($id)
+    {
+        $record = $this->getById($id);
+
+        return $record->delete();
+    }
+}
