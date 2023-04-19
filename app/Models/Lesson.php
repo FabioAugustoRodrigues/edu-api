e4f710f4e45161c16c355fb66586ee8dd79c1ceb
Fábio Augusto Rodrigues <69022068+FabioAugustoRodrigues@users.noreply.github.com>
2023-04-19 10:47:35 -0300

    feat: created Lesson entity

--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -5,13 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Enrollment extends Model
+class Note extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'student_id',
         'course_id',
-        'status'
+        'name',
+        'lesson_order'
     ];
 }
---
+++ database/migrations/2023_04_19_133730_create_lessons_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLessonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lessons', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('course_id');
+            $table->string('name');
+            $table->integer('lesson_order');
+            $table->timestamps();
+
+            $table->unique(['course_id', 'lesson_order']);
+
+            $table->foreign('course_id')
+                ->references('id')
+                ->on('courses')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lessons');
+    }
+}
